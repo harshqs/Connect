6df544e060f7f6be8b458f9bdea8fe4c44464f6f
Developer Anant Tiwari <dr.agasthya@gmail.com>
2026-08-27 16:13:30 +0530

    refactor: remove vscode extension and focus on core real-time web platform features

--- src/app/doc/[id]/page.tsx
+++ src/app/doc/[id]/page.tsx
@@ -20,7 +20,6 @@ import { VersionHistory } from "@/components/editor/VersionHistory";
 import { CommentSidebar } from "@/components/editor/CommentSidebar";
 import { CollaborativeCanvas } from "@/components/canvas/CollaborativeCanvas";
 import { ResearchPanel } from "@/components/research/ResearchPanel";
-import { VSCodeBridge } from "@/components/vscode/VSCodeBridge";
 import {
   DocumentItem, fetchDocumentById, fetchCurrentUser,
   updateDocument, Comment, DocumentVersion,
@@ -289,7 +288,6 @@ export default function DocumentEditorPage({ params }: { params: Promise<{ id: s
         currentUserName={currentUser.name}
       />
       {researchOpen && documentData && <ResearchPanel documentId={documentData.id} onClose={() => setResearchOpen(false)} />}
-      <VSCodeBridge documentId={docId} currentUser={currentUser} />
     </div>
   );
 }
